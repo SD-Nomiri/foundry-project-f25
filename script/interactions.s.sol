9fe0c6ec06f1a416dbc3ea2d44122787d3167150
SD_Nomiri <root@Dave>
2025-01-17 14:13:51 +0000

    third commit

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -1,50 +1,42 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.01 ether;
+    uint256 SEND_VALUE = 0.1 ether;
 
     function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
 
-        console.log("Funded FundMe contract with %s", SEND_VALUE);
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
     }
 }
 
 contract WithdrawFundMe is Script {
     function withdrawFundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
-
-        // Check the balance of the contract
-        uint256 contractBalance = address(mostRecentlyDeployed).balance;
-        console.log("Contract balance before withdrawal: %s", contractBalance);
-
-        // Check the balance of the sender
-        uint256 senderBalance = msg.sender.balance;
-        console.log("Sender balance before withdrawal: %s", senderBalance);
-
-        // Perform the withdrawal
         FundMe(payable(mostRecentlyDeployed)).withdraw();
-
-        // Check the balance of the contract after withdrawal
-        contractBalance = address(mostRecentlyDeployed).balance;
-        console.log("Contract balance after withdrawal: %s", contractBalance);
-
-        // Check the balance of the sender after withdrawal
-        senderBalance = msg.sender.balance;
-        console.log("Sender balance after withdrawal: %s", senderBalance);
-
         vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        vm.startBroadcast();
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
         withdrawFundMe(mostRecentlyDeployed);
-        vm.stopBroadcast();
     }
 }
